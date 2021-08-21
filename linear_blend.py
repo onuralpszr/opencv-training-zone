a6d6a9ae7a134cd63663d247e212c88d5f8f4ec8
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2021-08-21 10:49:11 +0300

    cosmetic fixes

--- linear_blend.py
+++ linear_blend.py
@@ -1,11 +1,13 @@
 import cv2 as cv
+import numpy as np
 
-ALPHA_SLIDER_MAX = 100
-TITLE_WINDOW = "Linear Blend"
+
+ALPHA_SLIDER_MAX: int = 100
+TITLE_WINDOW: str = "Linear Blend"
 
 # Image sizes has to be same
-SRC1 = cv.imread(cv.samples.findFile("LinuxLogo.jpg"))
-SRC2 = cv.imread(cv.samples.findFile("WindowsLogo.jpg"))
+SRC1: np.ndarray = cv.imread(cv.samples.findFile("LinuxLogo.jpg"))
+SRC2: np.ndarray = cv.imread(cv.samples.findFile("WindowsLogo.jpg"))
 
 
 def on_trackbar(val: int) -> None:
@@ -17,7 +19,7 @@ def on_trackbar(val: int) -> None:
 
 def main() -> None:
     cv.namedWindow(TITLE_WINDOW)
-    trackbar_name = f"Alpha x {ALPHA_SLIDER_MAX}"
+    trackbar_name: str = f"Alpha x {ALPHA_SLIDER_MAX}"
     cv.createTrackbar(trackbar_name, TITLE_WINDOW, 0, ALPHA_SLIDER_MAX, on_trackbar)
 
     # Set trackbar value to 0 to show the picture
